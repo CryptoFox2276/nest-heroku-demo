9806c0020abf924bad81799a535713348fc6ac27
jstopcoder <jstopcoder0209@gmail.com>
2022-11-13 14:26:46 +0400

    init project

---
+++ src/mock/products.mock.ts
@@ -0,0 +1,14 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        name: "First product",
+        description: "This is the description for the first product",
+        price: "200",
+    },
+    {
+        id: 2,
+        name: "Second product",
+        description: "This is the description for the second product",
+        price: "500",
+    }
+]
